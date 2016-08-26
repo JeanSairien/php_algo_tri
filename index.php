759c9d4c4b60001e165ed5a71f6819521f1410db
Redfish <lesonduko@gmail.com>
2016-08-26 11:31:08 +0200

    algo de tri en cours de resolution

---
+++ index.php
@@ -0,0 +1,51 @@
+<?php  
+
+/*TRi bulle php*/
+
+
+$tri_bulle = array('emp1 ' => '12 ' , // on declare notre tableau
+				   'emp2 ' => '54 ' ,
+				   'emp3 ' => '34 ' ,
+				   'emp5 ' => '18 ' ,
+				   'emp6 ' => '24 ' ,
+				   'emp7 ' => '78 ' ,
+				   'emp8 ' => '36 ' ,
+				   'emp9 ' => '59 ' ,
+				   'emp10 ' => '62 ' );
+				  					
+					
+
+
+/*sort($tri_bulle); // ici la fonction sort() tri notre tableau*/
+
+if()
+
+
+foreach ($tri_bulle as $key => $value){ // ici on parcour notre tableau apres tri
+ 	/*for($key=0;$key<10;$key++)*/
+ 	echo $value ; // et on laffiche
+ 		
+ 	}	
+ 
+ 
+
+?>
+
+
+
+<?php 
+//tri par insertion
+
+$tri_bulle = array(12,15,65,5,61,54,8,3,5,45);
+
+
+
+
+foreach ($tri_bulle as $key => $value){ // ici on parcour notre tableau apres tri
+ 	/*for($key=0;$key<10;$key++)*/
+ 	echo $value ; // et on laffiche
+ 		
+ 	}	
+
+
+?>
