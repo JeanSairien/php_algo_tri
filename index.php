dbdd10ae6401d0da9ddb4cb9af36f543fb753023
Redfish <lesonduko@gmail.com>
2016-09-05 17:16:04 +0200

    upload

--- index.php
+++ index.php
@@ -1,30 +1,30 @@
-<?php  
+<?php  /*
 
-/*TRi bulle php*/
+
 $tri_bulle = array(10,25,23,65,89,65,9,89,98,4);
 				  					
 					
 
 
-/*sort($tri_bulle); // ici la fonction sort() tri notre tableau*/
+/*sort($tri_bulle); // ici la fonction sort() tri notre tableau
 
 
-	for($key = 0 ; $key<10 ; $key++){
- 		for($j = 0 ; $j < 9; $j++){ 
-	 		if($tri_bulle[$j]>$tri_bulle[$j+1]){
+	for($key = 0 ; $key<10 ; $key++){ //on incremente pour parcourir le tableau 
+ 		for($j = 0 ; $j < 9; $j++){  //ici on incremente pour analyser notre tableau
+	 		if($tri_bulle[$j]>$tri_bulle[$j+1]){ // la notre condition traite l'ordre 
 	 			$temp = $tri_bulle[$j+1];
 	 			$tri_bulle[$j+1] = $tri_bulle[$j];
-	 			$tri_bulle[$j] = $temp;
+	 			$tri_bulle[$j] = $temp; // puis on stocke nos valeur a deplacer dans une variable temporaire pour mettre en ordre
 
 
  	 
- 	 			//echo $value ; // et on laffiche;
  			}
  		
- 		}
+ 	 			//echo $value ; 
  	
+ 		}
  	}
- 	print_r($tri_bulle);
+ 	print_r($tri_bulle);*/    // et on affiche;
 
 
 
@@ -32,19 +32,26 @@ $tri_bulle = array(10,25,23,65,89,65,9,89,98,4);
 
 
 
-<!--<?php 
-/*tri par insertion
+<?php 
+/*tri par insertion*/
 
-$tri_insert = array(12,15,65,5,61,54,8,3,5,45);
+$tri_insert = array(10,25,23,65,89);
 
+$i=3;
 
-
-
-foreach ($tri_bulle as $key => $value){  ici on parcour notre tableau apres tri
- 	/*for($key=0;$key<10;$key++)
- 	echo $value ; // et on laffiche
+/*for($i = 0 ; $i<10 ; $i++){ // ici on parcour notre tableau*/
+	while($tri_insert[$i] > $tri_insert[$i+1]){
+		
+				$temp = $tri_insert[$i+1];
+	 			$tri_insert[$i+1] = $tri_insert[$i];
+	 			$tri_insert[$i] = $temp;
+ 	 
+ 	 print_r($tri_insert); // et on laffiche
+	
+	}	
+ 	
+	
  		
- 	}	
+ 	 
 
-
-?>*/
+?>
